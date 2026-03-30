df2eb65675114c12d4c38c08f2d801ee9bdabb93
amitisdeveloper <amitisdeveloper@gmail.com>
2026-03-30 23:28:24 +0530

    url of api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
-export const API_BASE = import.meta.env.DEV 
-  ? 'http://localhost:3001/api' 
-  : (import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : '/api');
+export const API_BASE = import.meta.env.DEV
+  ? 'http://localhost:3001/api'
+  : '/api';
 
 export const apiCall = async (endpoint: string, options: RequestInit = {}) => {
   const url = `${API_BASE}${endpoint}`;
