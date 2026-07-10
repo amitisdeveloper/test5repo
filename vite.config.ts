95f5892bcc806e361795a28da4b015130bf1ed1c
amitisdeveloper <amitisdeveloper@gmail.com>
2026-07-10 07:58:37 +0530

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,5 @@ export default defineConfig({
         secure: false
       }
     }
-  },
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://localhost:3001')
   }
 })
